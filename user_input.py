225256d0638a30ccb3cd64592cdfe2b2ece9c533
23MorganDev <morganmuthee1@gmail.com>
2024-08-20 17:21:25 +0300

    input() function

---
+++ user_input.py
@@ -0,0 +1,6 @@
+# input fuction
+
+Name = input("Please enter your name: ")
+Age = input("Please enter your age: ")
+Location = input("Enter your current location: ")
+print("Hello, my name is " + Name + ". I am " + Age + " years of age. I live and work in " + Location + ".")
